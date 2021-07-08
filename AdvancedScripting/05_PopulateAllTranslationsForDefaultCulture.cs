86aa7503bc00e1c4e0491820413f7217f96841d8
Ted Pattison <ted@tedpattison.net>
2021-07-08 17:05:46 -0400

    Updates

---
+++ AdvancedScripting/05_PopulateAllTranslationsForDefaultCulture.cs
@@ -0,0 +1,21 @@
+foreach(Table table in Model.Tables) {     
+    if(!table.IsHidden){
+       // generate translation for table name
+       table.TranslatedNames[Model.Culture] = table.Name;
+     
+       foreach (Column column in table.Columns) {
+           // generate translation for column name
+           column.TranslatedNames[Model.Culture] = column.Name;
+       };
+
+       foreach (Measure measure in table.Measures) {
+           // generate translation for measure name
+           measure.TranslatedNames[Model.Culture] = measure.Name;
+       }; 
+
+       foreach (Hierarchy hierarchy in table.Hierarchies) {
+           // generate translation for hierarchy name
+           hierarchy.TranslatedNames[Model.Culture] = hierarchy.Name;
+       };
+    }
+}
